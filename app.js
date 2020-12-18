71a8ef55c7c98c91c8a40b8ba5ee5e178bfe254d
Erik Tangvik <erikjt@outlook.com>
2020-12-18 09:46:15 -0600

    notes for what app.js needs to do

---
+++ app.js
@@ -0,0 +1,13 @@
+const priceChanger = document.getElementById("priceChanger");
+
+priceChanger.addEventListener("change", () => {
+  console.log("so far so good");
+});
+
+// Things that have to change
+
+//toggle switch needs opacity to lighten when monthly
+
+//#basicPlan needs to change to >  &dollar;19.99
+//#professionalPlan needs to change to >  &dollar;24.99
+//#masterPlan needs to change to >  &dollar;39.99
