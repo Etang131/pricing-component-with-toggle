a5a0f53f51a06b79a952c9e90d601e914cdfd75d
Erik Tangvik <erikjt@outlook.com>
2020-12-19 08:58:03 -0600

    javascript works

--- app.js
+++ app.js
@@ -1,13 +1,25 @@
 const priceChanger = document.getElementById("priceChanger");
 
+const labelLight = document.getElementById("labelLight");
+
+const basic = document.getElementById("basicPlan");
+const professional = document.getElementById("professionalPlan");
+const master = document.getElementById("masterPlan");
+
+const money = "$";
+
 priceChanger.addEventListener("change", () => {
-  console.log("so far so good");
+  if (basic.textContent === `${money}199.99`) {
+    console.log("so far so good");
+    labelLight.setAttribute("style", "opacity: 0.5");
+    basic.textContent = `${money}19.99`;
+    professional.textContent = `${money}24.99`;
+    master.textContent = `${money}39.99`;
+  } else {
+    console.log("The cows are coming home boys");
+    labelLight.setAttribute("style", "opacity: 1.00");
+    basic.textContent = `${money}199.99`;
+    professional.textContent = `${money}249.99`;
+    master.textContent = `${money}399.99`;
+  }
 });
-
-// Things that have to change
-
-//toggle switch needs opacity to lighten when monthly
-
-//#basicPlan needs to change to >  &dollar;19.99
-//#professionalPlan needs to change to >  &dollar;24.99
-//#masterPlan needs to change to >  &dollar;39.99
